95e9e13cc685645ac2a3ef5d343ea75f0f65bf29
Rich Prohaska <prohaska@tokutek.com>
2013-08-21 15:40:31 -0400

    #50 count lock tree timeouts

--- locktree/locktree.h
+++ locktree/locktree.h
@@ -332,6 +332,8 @@ public:
 
         struct lt_counters m_lt_counters;
 
+        struct lt_counters m_lt_counters;
+
         // lock wait time for blocking row locks, in ms
         uint64_t m_lock_wait_time_ms;
 
